f2794daf24fd0abc7d210946d3c6fd0473c0ff03
Carlos Zabala <czabala847@gmail.com>
2023-08-05 13:31:44 -0500

    create routes

--- src/pages/App/index.tsx
+++ src/pages/App/index.tsx
@@ -1,7 +1,28 @@
+import { BrowserRouter, useRoutes } from "react-router-dom";
+import { Home } from "../Home";
+import { MyOrders } from "../MyOrders";
+import { MyAccount } from "../MyAccount";
+import { MyOrder } from "../MyOrder";
+import { SignIn } from "../SignIn";
+import { NotFound } from "../NotFound";
+
+const AppRoutes = () => {
+  const routes = useRoutes([
+    { path: "/", element: <Home /> },
+    { path: "/my-account", element: <MyAccount /> },
+    { path: "/my-order", element: <MyOrder /> },
+    { path: "/my-orders", element: <MyOrders /> },
+    { path: "/sign-in", element: <SignIn /> },
+    { path: "/*", element: <NotFound /> },
+  ]);
+
+  return routes;
+};
+
 export function App() {
   return (
-    <>
-      <h1>Hola mundo</h1>
-    </>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   );
 }
